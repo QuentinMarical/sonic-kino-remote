81a52223f43969b34587a61b9c73c19286aadfb5
NitraFox <nitrafoxmail@gmail.com>
2020-05-11 18:11:24 +0200

    Initial commit

---
+++ lua/sonicsd/sonics/sonickinoremote.lua
@@ -0,0 +1,7 @@
+SonicSD:AddSonic({
+	ID="sonickinoremote",
+	Name="The kino remote from Stargate Universe",
+	ViewModel="models/Iziraider/kinoremote/v_kinoremote.mdl",
+	WorldModel="models/Iziraider/kinoremote/w_kinoremote.mdl",
+	SoundLoop = "kino/kino_sonicloop.wav"
+})
